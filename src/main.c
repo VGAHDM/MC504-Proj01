d5114c642875de21527065753d52718595944bc5
Artur Hendler <arturhendler@hotmail.com>
2022-04-09 12:27:02 -0300

    main.c - fixed

--- src/main.c
+++ src/main.c
@@ -1,43 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     int xmax,ymax,zmax,numWords;
     FILE *file;
-    //char *matrix, *words;
     file = fopen("..\\input\\entrada.txt","r");
     
     fscanf(file, "%d %d %d %d", &xmax, &ymax, &zmax, &numWords);
 
-    //matrix = malloc(sizeof(char)*ymax*xmax*zmax);
-    //words =  malloc(sizeof(char)*100*numWords); //a word can not have more than 100 chars
-    
-    char words[numWords][100];
+    char words[numWords*2][100];
     char matrix[zmax][ymax][xmax];
     char* aux; 
+
     for(int i = 0; i < numWords; i++){
         fscanf(file, "%s", &words[i]);
-        //fgets(words[i],1024,file);
+        strcpy(words[i+numWords],words[i]);
+        strrev(words[i+numWords]);
     }
-    for(int i = 0; i < numWords; i++){
-        printf("%s ",words[i]);
-    }
-    
+
     for(int z = 0; z < zmax; z++){
         for(int x = 0; x < xmax; x++){
-            //fscanf(file, "%c", matrix[y][x][z]);
             fscanf(file, "%s", matrix[z][x]);
 
         }
     }
-    for(int z = 0; z < zmax; z++){   
-        for(int x = 0; x < xmax; x++){
-            printf("%s\n", matrix[z][x]);
-            
-            //fscanf(file, "%s", matrix[x][z]);
+
+    /* for(int i = 0; i < numWords*2; i++){
+        printf("%s ",words[i]);
+    } */
+    /*
+    for(int z = 0; z < zmax; z++){
+        for(int y = 0; y < ymax; y++){   
+            for(int x = 0; x < xmax; x++){
+                printf("%c", matrix[z][y][x]);
+                
+                //fscanf(file, "%s", matrix[x][z]);
+            }
+            printf("\n");
         }
-        printf("\n");
-        }
+    } */
+
+    //x,y,z,xy,xz,yz,xyz
     return 0;
 }
