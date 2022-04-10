670c4d1fbd4344f9e1a107c926f4e37d4dcaf13f
Artur Hendler <arturhendler@hotmail.com>
2022-04-09 23:26:15 -0300

    stuff fixed

--- src/main.c
+++ src/main.c
@@ -2,46 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct struct_args {
+    char * words;
+    char * matrix;
+    int xmax;
+    int ymax;
+    int zmax;
+} struct_args;
+
 int main()
 {
     int xmax,ymax,zmax,numWords;
     FILE *file;
     file = fopen("..\\input\\entrada.txt","r");
     
-    fscanf(file, "%d %d %d %d", &xmax, &ymax, &zmax, &numWords);
+    fscanf(file, "%d %d %d %d\n", &xmax, &ymax, &zmax, &numWords);
 
-    char words[numWords*2][100];
-    char matrix[zmax][ymax][xmax];
-    char* aux; 
+    char * words;
+    char * matrix; 
 
+    words = malloc(sizeof(char)*100*numWords*2);
+    matrix = malloc(sizeof(char)*zmax*ymax*xmax);
+    
     for(int i = 0; i < numWords; i++){
-        fscanf(file, "%s", &words[i]);
-        strcpy(words[i+numWords],words[i]);
-        strrev(words[i+numWords]);
+        fgets((words+i*100),98,file);
+        (words+i*100)[strcspn((words+i*100), "\n")] = 0;
+        strcpy((words+(i+numWords)*100),(words+i*100));
+        strrev((words+(i+numWords)*100)); 
     }
-
+ 
     for(int z = 0; z < zmax; z++){
         for(int x = 0; x < xmax; x++){
-            fscanf(file, "%s", matrix[z][x]);
+            fgets((matrix+(x*xmax)+(z*xmax*ymax)),xmax+2,file);
+            (matrix+(x*xmax)+(z*xmax*ymax))[strcspn((matrix+(x*xmax)+(z*xmax*ymax)), "\n")] = 0;
 
         }
-    }
+    } 
+ 
 
-    /* for(int i = 0; i < numWords*2; i++){
-        printf("%s ",words[i]);
-    } */
-    /*
-    for(int z = 0; z < zmax; z++){
+    /* for(int z = 0; z < zmax; z++)// printa a matriz
         for(int y = 0; y < ymax; y++){   
             for(int x = 0; x < xmax; x++){
-                printf("%c", matrix[z][y][x]);
-                
-                //fscanf(file, "%s", matrix[x][z]);
+                printf("%c", *(matrix+(z*xmax*ymax)+(y*xmax)+x));
             }
             printf("\n");
         }
-    } */
-
+    } */ 
+    /* struct_args var; //print do dicionario e teste de struct
+    var.words = words;
+    for(int i = 0; i < numWords*2; i++){
+        printf("%s-",(var.words+i*100));
+    }  */
     //x,y,z,xy,xz,yz,xyz
     return 0;
 }
